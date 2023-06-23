937bb03b6b9fc88515b76acbd5562da4709e3691
mahmud139 <mahmudulhasan139139@gmail.com>
2023-06-23 21:42:34 +0800

    updated main.go

--- main.go
+++ main.go
@@ -10,13 +10,17 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
-	
+
 	log.Println("Starting server on: localhost:8080")
 	err := http.ListenAndServe("localhost:8080", mux)
 	log.Fatal(err)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from snippet box"))
 }
 
