2811af33137d1f579cd2b5b2b97b69346eea690b
mahmud139 <mahmudulhasan139139@gmail.com>
2023-06-24 20:01:46 +0800

    update showsnippet handler

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -25,7 +27,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("display a specific snippet"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil && id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "id: %v", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
