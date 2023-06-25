8dadf9583eadc44372aef6ac3894ec93c4e9884e
mahmud139 <mahmudulhasan139139@gmail.com>
2023-06-25 11:19:29 +0800

    updated main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,9 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	fileServer := http.FileServer(http.Dir("M:/Projects/Snippet_box/ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	
 	log.Println("Starting server on: localhost:8080")
 	err := http.ListenAndServe("localhost:8080", mux)
