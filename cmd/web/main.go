2f3257bb7c91db75c8ee7da6bf58d7f599b686ff
mahmud139 <mahmudulhasan139139@gmail.com>
2023-06-25 22:35:49 +0800

    added custom log

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,16 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", "localhost:8080", "HTTP network address")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -17,7 +22,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("M:/Projects/Snippet_box/ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	
-	log.Printf("Starting server on: %v\n", *addr)
+	infoLog.Printf("Starting server on %v\n", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
