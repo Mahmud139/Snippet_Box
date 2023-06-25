2c6c3e0d54a756c1a37760a5122a8f55af8b431f
mahmud139 <mahmudulhasan139139@gmail.com>
2023-06-25 21:42:18 +0800

    added HTTP network address flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", "localhost:8080", "HTTP network address")
+	flag.Parse()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
-mux.
+
 	fileServer := http.FileServer(http.Dir("M:/Projects/Snippet_box/ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	
-	log.Println("Starting server on: localhost:8080")
-	err := http.ListenAndServe("localhost:8080", mux)
+	log.Printf("Starting server on: %v\n", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
