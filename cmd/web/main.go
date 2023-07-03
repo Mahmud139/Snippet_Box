0b20d18c6fbcd9e397d4be7bfee4fbd5c8c91b3a
mahmud139 <mahmudulhasan139139@gmail.com>
2023-07-04 02:28:35 +0800

    added mysql driver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,6 +17,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", "localhost:8080", "HTTP network address")
+	dsn := flag.String("dsn", "web:mahmud@/snippetbox?parseTime=true", "MySql data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -33,4 +37,17 @@ func main() {
 	infoLog.Printf("Starting server on %v\n", *addr)
 	err := server.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err = nil {
+		return nil, err
+	}
+	
+	return db, nil
 }
