30558d05fdb33056ce2bf0d29496a3e54d3c5209
Mahmud139 <mahmudulhasan139139@gmail.com>
2023-07-11 22:44:33 +0800

    updated templates.go

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"html/template"
 	"path/filepath"
 
@@ -11,6 +12,14 @@ type templateData struct {
 	CurrentYear int
 	Snippet *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate" : humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -24,7 +33,12 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err= ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
